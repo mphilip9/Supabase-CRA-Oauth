79be2c0046598658fdafea88f6ad96bb297f253f
Maxwell Philip <max.philip1@gmail.com>
2023-01-20 15:13:33 -0500

    add serve command

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/ping", function (req, res) {
-  return res.send("pong");
+  return res.send("bingo");
 });
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
