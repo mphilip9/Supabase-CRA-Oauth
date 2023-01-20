fb4ac8788c6a13e0a83b81dcf1fb75d596f0279c
Maxwell Philip <max.philip1@gmail.com>
2023-01-20 12:07:35 -0500

    server working locally

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
