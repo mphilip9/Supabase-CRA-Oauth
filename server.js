30d888ac378b7e493c7dba2319c9e25bba8d0850
Maxwell Philip <max.philip1@gmail.com>
2023-01-20 12:01:51 -0500

    middleware reconfiguration

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/ping", function (req, res) {
 // app.get("/", function (req, res) {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
