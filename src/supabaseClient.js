5931c57b48385dd0790a7e719bf1a014b7b5e4ce
Maxwell Philip <max.philip1@gmail.com>
2023-01-20 11:57:50 -0500

    remove test for env variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const testing = process.env.REACT_APP_TEST;
-console.log(testing);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
